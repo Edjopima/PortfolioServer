a70cc226fed92f4dbed4be92e454ec08dd8eed4f
Eduardo Piña <mrejpm@gmail.com>
2020-12-29 20:07:49 -0400

    [Add] send email route and function

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const {google} = require('googleapis');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+  console.log(`we are live on port ${process.env.PORT}`);
+});
+
+const CLIENT_ID=process.env.CLIENT_ID
+const CLIENT_SECRET=process.env.CLIENT_SECRET
+const REDIRECT_URI=process.env.REDIRECT_URI
+const REFRESH_TOKEN=process.env.REFRESH_TOKEN
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI,
+  )
+
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
+
+const sendMail = async ({name,email,subject,message}) => {
+  try {
+    const accessToken = await oAuth2Client.getAccessToken()
+    const smtpTransport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type:'OAuth2',
+        user:process.env.GMAIL_USER,
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accesToken:accessToken
+        }
+      });
+
+      const mailOptions = {
+        from: `Portfolio <${process.env.GMAIL_USER}>`,
+        to: process.env.EMAIL_TO_SEND,
+        subject: subject,
+        html: `<p>${name}</p>
+              <p>${email}</p>
+              <p>${message}</p>`
+      };
+      const result = await smtpTransport.sendMail(mailOptions);
+      return result;
+    } catch (err){
+      console.log(err);
+    }
+
+  }
+
+app.post('/sendMail', (req,res) => {
+  sendMail(req.body)
+    .then(result=>res.status(200).json('Send'))
+    .catch(err=>console.log(err))
+})
